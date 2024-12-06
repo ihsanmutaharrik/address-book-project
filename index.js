bc42365881dfc16bb309200050af19ed469f4693
Muhammad Ihsan M <muhammadihsangit@gmail.com>
2024-12-06 21:08:10 +0700

    Modified Indentation

--- index.js
+++ index.js
@@ -1,25 +1,25 @@
 let contacts = [
-    {
+  {
     id: 1,
-    Name: "contact1",
-    Email: "contac1@gmail.com",
-    Phone: "1111111111",
-    Company: "company 1"
-    },
-    {
+    name: "contact1",
+    email: "contac1@gmail.com",
+    phone: "1111111111",
+    company: "company 1",
+  },
+  {
     id: 2,
-    Name: "contact2",
-    Email: "contac2@gmail.com",
-    Phone: "2222222222",
-    Company: "company 2"
-    },
-    {
+    name: "contact2",
+    email: "contac2@gmail.com",
+    phone: "2222222222",
+    company: "company 2",
+  },
+  {
     id: 3,
-    Name: "contact3",
-    Email: "contac3@gmail.com",
-    Phone: "3333333333",
-    Company: "company 3"
-    },
+    name: "contact3",
+    email: "contac3@gmail.com",
+    phone: "3333333333",
+    company: "company 3",
+  },
 ];
 
-console.log({ contacts});
+console.log({ contacts });
