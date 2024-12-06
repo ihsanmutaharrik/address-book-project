318ead42920d11c8503d772e48384be5cff37a29
Muhammad Ihsan M <muhammadihsangit@gmail.com>
2024-12-06 21:35:40 +0700

    Fix dummy Data

--- index.js
+++ index.js
@@ -1,24 +1,24 @@
 let contacts = [
   {
     id: 1,
-    name: "contact1",
-    email: "contac1@gmail.com",
-    phone: "1111111111",
-    company: "company 1",
+    name: "Muhammad Haidar Hanif",
+    email: "haidar@bearmentor.com",
+    phone: "0881-8881-8881",
+    company: "Bearmentor Group",
   },
   {
     id: 2,
-    name: "contact2",
-    email: "contac2@gmail.com",
-    phone: "2222222222",
-    company: "company 2",
+    name: "Isyana Sarasvati",
+    email: "Isyana@gmail.com",
+    phone: "0882-8882-8882",
+    company: "Redrose Recording",
   },
   {
     id: 3,
-    name: "contact3",
-    email: "contac3@gmail.com",
-    phone: "3333333333",
-    company: "company 3",
+    name: "Raul Gonzales",
+    email: "gonzalesraul@gmail.com",
+    phone: "0877-7772-7771",
+    company: "Real Madrid Company",
   },
 ];
 
