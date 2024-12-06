f52a7ba662c0330f54e701aeaeafd1c514efb2c0
Muhammad Ihsan M <muhammadihsangit@gmail.com>
2024-12-06 19:06:42 +0700

    add Index.JS

---
+++ index.js
@@ -0,0 +1,25 @@
+let contacts = [
+    {
+    id: 1,
+    Name: "contact1",
+    Email: "contac1@gmail.com",
+    Phone: "1111111111",
+    Company: "company 1"
+    },
+    {
+    id: 2,
+    Name: "contact2",
+    Email: "contac2@gmail.com",
+    Phone: "2222222222",
+    Company: "company 2"
+    },
+    {
+    id: 3,
+    Name: "contact3",
+    Email: "contac3@gmail.com",
+    Phone: "3333333333",
+    Company: "company 3"
+    },
+];
+
+console.log({ contacts});
