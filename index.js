f1db502b4c0a3dce7d2c14f8e4da750a2e2738e3
Muhammad Ihsan M <muhammadihsangit@gmail.com>
2024-12-13 19:32:20 +0700

    add : testing add contact

--- index.js
+++ index.js
@@ -1,20 +1,17 @@
 const contacts = [
   {
-    id: 1,
     name: "Muhammad Haidar Hanif",
     email: "haidar@bearmentor.com",
     phone: "0881-8881-8881",
     company: "Bearmentor Group",
   },
   {
-    id: 2,
     name: "Isyana Sarasvati",
     email: "Isyana@gmail.com",
     phone: "0882-8882-8882",
     company: "Redrose Recording",
   },
   {
-    id: 3,
     name: "Raul Gonzales",
     email: "gonzalesraul@gmail.com",
     phone: "0877-7772-7771",
@@ -26,9 +23,36 @@ const contacts = [
 function showContacts() {
   for (let index = 0; index < contacts.length; index++) {
     const tempContact = contacts[index];
-    const outputContact = `${tempContact.name} (${tempContact.phone}) ${tempContact.email} ${tempContact.company}`;
-    console.log(outputContact);
+    const outputContact = `ID : ${index + 1} ${tempContact.name} (${
+      tempContact.phone
+    }) ${tempContact.email} ${tempContact.company}`;
+    alert(outputContact);
   }
 }
 
-showContacts();
+function testAddContact() {
+  const testContact = {
+    name: "Cristiano Ronaldo",
+    email: "Cristianoronaldo@gmail.com",
+    phone: "0877-7654-3210",
+    company: "Al Nassr Company",
+  };
+  contacts.push(testContact);
+}
+
+
+function showMenu() {
+  let choice = prompt("1. Test Show Contact\n" + "2. Test Add Contact");
+
+  switch (choice) {
+    case "1":
+      showContacts();
+      break;
+    case "2":
+      testAddContact();
+      showContacts();
+      break;
+  }
+}
+
+showMenu()
